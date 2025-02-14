38d37694e7e5f3c4287fcc853f84a3c971818ee3
Rafael Nico T. Maniquiz <rtmaniquiz@protonmail.com>
2025-02-14 12:06:26 +0800

    Add files via upload

---
+++ dashboard_mockup.py
@@ -0,0 +1,8 @@
+import dash
+from dash import html
+from dash import dcc
+from dash.dash_table import DataTable
+import dash_bootstrap_components as dbc
+import pandas as pd
+import plotly.express as px
+
